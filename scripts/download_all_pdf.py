df3173a00092867760494b255bdd3f352c5e6f4c
Kexin Xie <ikexinxie@gmail.com>
2013-03-31 14:37:36 +1100

    - fix the script - add virtualenv directory to gitignore

--- scripts/download_all_pdf.py
+++ scripts/download_all_pdf.py
@@ -1,10 +1,11 @@
 import sys
-from scrape.download_files import get_internet_files
-from pdf_files import get_all_pdfs
+from pylibstraw.scrape.download_files import get_internet_files
+from pylibstraw.scrape.pdf_files import get_all_pdfs
 
 if __name__ == "__main__":
     html_file = sys.argv[1]
     target_location = sys.argv[2]
 
-    pdf_links = get_all_pdfs(html_file)
-    get_internet_files(pdf_links, target_location)
+    with open(html_file, 'r') as html_content:
+        pdf_links = get_all_pdfs(html_content)
+        get_internet_files(pdf_links, target_location)
